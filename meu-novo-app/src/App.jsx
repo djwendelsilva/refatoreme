a568e9323550be4c7d15dfd7845ab6d67a994ff8
djwendelsilva <wendel.j.silva7@aluno.senai.br>
2026-04-01 14:55:53 -0300

    funcionando com Footer corrigido

--- meu-novo-app/src/App.jsx
+++ meu-novo-app/src/App.jsx
@@ -1,15 +1,21 @@
 import Header from './components/Header'
 import Card from './components/card'
 import Footer from './components/Footer'
+import './App.css'
 
 function App() {
   return (
     <div>
-      <Header />
-      <Card titulo="Projeto 1" descricao="Minha primeira refatoração" />
-      <Card titulo="Projeto 2" descricao="Usando Docker com React" />
-      <Footer />
+      <Header /> {/* Isso faz o Header aparecer */}
+      
+      <main>
+        <Card titulo="Meu Primeiro Projeto" descricao="Refatoração concluída!" />
+        <Card titulo="Docker" descricao="Configurado com sucesso." />
+      </main>
+
+      <Footer /> {/* Isso faz o Footer aparecer */}
     </div>
   )
 }
+
 export default App
---
+++ meu-novo-app/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer style={{ textAlign: 'center', padding: '10px', background: '#eee' }}>
+      <p>&copy; 2026 - Desenvolvido por djwendelsilva</p>
+    </footer>
+  )
+}
+
+// ESSA LINHA ABAIXO É A QUE ESTÁ FALTANDO:
+export default Footer; 
